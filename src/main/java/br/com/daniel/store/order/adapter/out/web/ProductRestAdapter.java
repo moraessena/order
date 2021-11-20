80094e71404ec3e78cc0d2035428736fd94f0d78
daniel <danielsena04@gmail.com>
2021-11-20 16:24:52 -0300

    config: setup service discovery client

--- src/main/java/br/com/daniel/store/order/adapter/out/web/ProductRestAdapter.java
+++ src/main/java/br/com/daniel/store/order/adapter/out/web/ProductRestAdapter.java
@@ -6,7 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "catalog", path = "/products", url = "${catalog.url}")
+@FeignClient("catalog")
 interface ProductRestAdapter {
 
     @GetMapping(value = "/{id}", consumes = {MediaType.APPLICATION_JSON_VALUE})
